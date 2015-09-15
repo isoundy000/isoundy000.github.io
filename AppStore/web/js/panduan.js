7c0e412a015325c7da2c9d83f0db2e2e0a63012a
isoundy000 <yygn000@126.com>
2015-09-15 21:12:44 +0800

    myAppStore1.3.2
    修正封皮美女层次

--- AppStore/web/js/panduan.js
+++ AppStore/web/js/panduan.js
@@ -52,7 +52,7 @@ $(document).ready(function(){
 				$(".wrapper").css("display","none");
 			});
 		}else{
-			$(".ua").attr( "href","itms-services://?action=download-manifest&url=https://ssl.pre.im/app/plist/53d3034089f6d7e27c79eb9268490f17" );
+			$(".ua").attr( "href","itms-services://?action=download-manifest&url=https://ssl.pre.im/app/plist/f878a1b1139793b0099d5cb86c5d3e04" );
 		}
 	}
 	if(Android){
@@ -77,7 +77,7 @@ $(document).ready(function(){
 		$(".ua").attr("href","/AppStore/TexasHoldem-debug.apk" );
 		$(".uaAppN").addClass("iOSPic");
 		$(".uaAppN").removeClass("AndPic,iOSPicM");
-		$(".uaN").attr("href","itms-services://?action=download-manifest&url=https://ssl.pre.im/app/plist/53d3034089f6d7e27c79eb9268490f17" );
+		$(".uaN").attr("href","itms-services://?action=download-manifest&url=https://ssl.pre.im/app/plist/f878a1b1139793b0099d5cb86c5d3e04" );
 	}
 });
 
