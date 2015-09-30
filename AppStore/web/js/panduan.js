2186b8b6f387f9361cbf797a37d4ca31ee62db77
isoundy000 <yygn000@126.com>
2015-09-30 13:05:22 +0800

    myAppStore1.0.3
    上线

--- AppStore/web/js/panduan.js
+++ AppStore/web/js/panduan.js
@@ -52,7 +52,7 @@ $(document).ready(function(){
 				$(".wrapper").css("display","none");
 			});
 		}else{
-			$(".ua").attr( "href","itms-services://?action=download-manifest&url=https://ssl.pre.im/app/plist/b3b4f852ead1a458cd8cd7b1636a859d" );
+			$(".ua").attr( "href","itms-services://?action=download-manifest&url=https://ssl.pre.im/app/plist/4eb76cb2bd3d4325ffc34bbb3fa8495d" );
 		}
 	}
 	if(Android){
@@ -77,7 +77,7 @@ $(document).ready(function(){
 		$(".ua").attr("href","/AppStore/TexasHoldem-debug.apk" );
 		$(".uaAppN").addClass("iOSPic");
 		$(".uaAppN").removeClass("AndPic,iOSPicM");
-		$(".uaN").attr("href","itms-services://?action=download-manifest&url=https://ssl.pre.im/app/plist/b3b4f852ead1a458cd8cd7b1636a859d" );
+		$(".uaN").attr("href","itms-services://?action=download-manifest&url=https://ssl.pre.im/app/plist/4eb76cb2bd3d4325ffc34bbb3fa8495d" );
 	}
 });
 
